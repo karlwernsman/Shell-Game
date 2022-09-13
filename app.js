6cd1ff4de5e37d4c6f6dec325e78ef0f2ea2c638
karl wernsman <wernsmank@gmail.com>
2022-09-12 18:12:49 -0700

    Begining of adding states

--- app.js
+++ app.js
@@ -2,15 +2,79 @@
 // import { getRandomItem } from './utils.js';
 
 /* State */
+let gameState = 'guess'; //'guess' or 'results'
+let guess = ''; //'guess-1', 'guess-2', or 'guess-3'
+let pearl = ''; //'pearl-1', 'pearl-2', or 'pearl-3'
+//let reveal = 'pearl'; // ?? 'pearl' or 'pearl-hidden'
+//let results = ''; // 'win' or 'lose'
+//let answer = 1;
 
 /* Actions */
-function loadPage() {}
+function loadPage() {
+    displayShells();
+}
+
+// function revealShells(userGuess) {
+//     gameState = '';
+//     guess = userGuess;
+// }
 
 /* Components */
 
-/* Component */
+/* Scoreboard */
+
+/* Guess */
 // get DOM
+const guess1 = document.getElementById('guess-1');
+const guess2 = document.getElementById('guess-2');
+const guess3 = document.getElementById('guess-3');
+const shell1 = document.getElementById('shell-1');
+const shell2 = document.getElementById('shell-2');
+const shell3 = document.getElementById('shell-3');
+const pearl1 = document.getElementById('pearl-1');
+const pearl2 = document.getElementById('pearl-2');
+const pearl3 = document.getElementById('pearl-3');
+const display1 = document.getElementById('display-1');
+const display2 = document.getElementById('display-2');
+const display3 = document.getElementById('display-3');
+const guessDiv = document.getElementById('guesses');
+const resultsDiv = document.getElementById('results');
+
 // display
+function displayShells() {
+    // guess1.classList.remove('win', 'lose');
+    // guess2.classList.remove('win', 'lose');
+    // guess3.classList.remove('win', 'lose');
+    shell1.classList.remove('reveal');
+    shell2.classList.remove('reveal');
+    shell3.classList.remove('reveal');
+    // if (gameState === 'guess') {
+    //     if (pearl === 'pearl-1') {
+    //     }
+    // }
+
+    if (gameState === 'guess') {
+        shell1.classList.remove('reveal');
+        shell2.classList.remove('reveal');
+        shell3.classList.remove('reveal');
+        resultsDiv.classList.add('hidden');
+    } else {
+        if (guess === 'guess-1') {
+            shell1.classList.add('reveal');
+            pearl1.classList.remove('hidden');
+        }
+        if (guess === 'guess-2') {
+            shell2.classList.add('reveal');
+            pearl2.classList.remove('hidden');
+        }
+        if (guess === 'guess-3') {
+            shell3.classList.add('reveal');
+            pearl3.classList.remove('hidden');
+        } else {
+            guessDiv.classList.add('hidden');
+        }
+    }
+}
 // event listeners
 
 /* Run page load code */
