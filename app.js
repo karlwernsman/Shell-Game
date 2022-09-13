12139575f7eb232b81e113d4d4d390364809f1e1
karl wernsman <wernsmank@gmail.com>
2022-09-12 20:07:28 -0700

    States complete

--- app.js
+++ app.js
@@ -2,9 +2,9 @@
 // import { getRandomItem } from './utils.js';
 
 /* State */
-let gameState = 'guess'; //'guess' or 'results'
-let guess = ''; //'guess-1', 'guess-2', or 'guess-3'
-let pearl = ''; //'pearl-1', 'pearl-2', or 'pearl-3'
+let gameState = 'results'; //'guess' or 'results'
+let guess = 'guess-3'; //'guess-1', 'guess-2', or 'guess-3'
+//let pearl = 'shell-1'; //'pearl-1', 'pearl-2', or 'pearl-3'
 //let reveal = 'pearl'; // ?? 'pearl' or 'pearl-hidden'
 //let results = ''; // 'win' or 'lose'
 //let answer = 1;
@@ -13,11 +13,6 @@ let pearl = ''; //'pearl-1', 'pearl-2', or 'pearl-3'
 function loadPage() {
     displayShells();
 }
-
-// function revealShells(userGuess) {
-//     gameState = '';
-//     guess = userGuess;
-// }
 
 /* Components */
 
@@ -28,30 +23,28 @@ function loadPage() {
 const guess1 = document.getElementById('guess-1');
 const guess2 = document.getElementById('guess-2');
 const guess3 = document.getElementById('guess-3');
+
 const shell1 = document.getElementById('shell-1');
 const shell2 = document.getElementById('shell-2');
 const shell3 = document.getElementById('shell-3');
+
 const pearl1 = document.getElementById('pearl-1');
 const pearl2 = document.getElementById('pearl-2');
 const pearl3 = document.getElementById('pearl-3');
+
 const display1 = document.getElementById('display-1');
 const display2 = document.getElementById('display-2');
 const display3 = document.getElementById('display-3');
+
 const guessDiv = document.getElementById('guesses');
 const resultsDiv = document.getElementById('results');
+const playAgainButton = document.getElementById('play-again-button');
 
 // display
 function displayShells() {
-    // guess1.classList.remove('win', 'lose');
-    // guess2.classList.remove('win', 'lose');
-    // guess3.classList.remove('win', 'lose');
     shell1.classList.remove('reveal');
     shell2.classList.remove('reveal');
     shell3.classList.remove('reveal');
-    // if (gameState === 'guess') {
-    //     if (pearl === 'pearl-1') {
-    //     }
-    // }
 
     if (gameState === 'guess') {
         shell1.classList.remove('reveal');
