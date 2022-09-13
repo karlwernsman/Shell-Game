7de6cf7bdecfe1dfc3a09f8f5ff07094ae60ecb7
karl wernsman <wernsmank@gmail.com>
2022-09-12 21:36:36 -0700

    Tried event listeners more

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 /* Imports */
-// import { getRandomItem } from './utils.js';
+//import { getRandomItem } from './utils.js';
 
 /* State */
 let gameState = 'guess'; //'guess' or 'results'
@@ -15,6 +15,10 @@ function playAgain() {
     gameState = 'guess';
     loadPage();
 }
+
+//const choices = ['pearl-1', 'pearl-2', 'pearl-3'];
+// }
+
 /* Components */
 
 /* Scoreboard */
@@ -33,9 +37,9 @@ const pearl1 = document.getElementById('pearl-1');
 const pearl2 = document.getElementById('pearl-2');
 const pearl3 = document.getElementById('pearl-3');
 
-const display1 = document.getElementById('display-1');
-const display2 = document.getElementById('display-2');
-const display3 = document.getElementById('display-3');
+//const display1 = document.getElementById('display-1');
+//const display2 = document.getElementById('display-2');
+//const display3 = document.getElementById('display-3');
 
 const guessDiv = document.getElementById('guesses');
 const resultsDiv = document.getElementById('results');
@@ -75,19 +79,20 @@ function displayShells() {
 // event listeners
 guess1.addEventListener('click', () => {
     guess = 'guess-1';
-    pearl = '';
+    pearl;
     gameState = 'results';
     displayShells(shell1);
 });
+
 guess2.addEventListener('click', () => {
     guess = 'guess-2';
-    pearl = '';
+    pearl;
     gameState = 'results';
     displayShells(shell2);
 });
 guess3.addEventListener('click', () => {
     guess = 'guess-3';
-    pearl = '';
+    pearl;
     gameState = 'results';
     displayShells(shell3);
 });
